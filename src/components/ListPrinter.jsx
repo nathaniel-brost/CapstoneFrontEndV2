aeee92622d0c99bd9ce5897a5780a0288960503e
Nathaniel Brost <NathanielBrost@NathaniBook2021.attlocal.net>
2022-03-16 22:41:03 -0500

    awesome universal mapper

--- src/components/ListPrinter.jsx
+++ src/components/ListPrinter.jsx
@@ -8,22 +8,43 @@ const Container = styled.div`
 
 `
 
+// new style component in charge of side by side
+const Spread = styled.div`
+    margin-right: 3rem;
+
+`
+
 const Card = styled.div`
-    display: grid;
-    place-content: center;
-    width: 30rem;
+    display: flex;
+    flex-wrap: wrap;
+    place-content: left;
+    width: 50rem;
     margin: 1rem;
-    padding: 7rem;
+    padding: 2rem 1rem;
     border-radius: 3px;
-    box-shadow: 3px 3 px 6px grey;
+    box-shadow: 3px 3px 6px grey;
+    background-color: aqua;
 
     `
-
-const ListPrinter = ({items, resourceName, itemComponent: ItemComponent}) => {
+// 1, 2, 3, 
+const ListPrinter = ({items, resourceName1, resourceName2, resourceName3, itemComponent1: ItemComponent1, itemComponent2: ItemComponent2, itemComponent3: ItemComponent3}) => {
     return ( 
         <>
         <Container>
-            {}
+            {items && items.map (item => (
+                <Card>
+                    <Spread>
+                    <ItemComponent1 {...{[resourceName1] : item}}/>
+                    </Spread>
+                    <Spread>
+                    <ItemComponent2 {...{[resourceName2] : item}}/>
+                    </Spread>
+                    <Spread>
+                    <ItemComponent3 {...{[resourceName3] : item}}/>
+                    </Spread>
+
+                </Card>
+            ) ) } 
 
         </Container>
 
