f9281c5016596eb80d493e2db06d8652746b92b7
Nathaniel Brost <NathanielBrost@NathaniBook2021.attlocal.net>
2022-03-16 22:39:58 -0500

    collection mapper component and calling on app

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import jwtDecode from 'jwt-decode';
 // component imports
 import Login from "./components/Login/login";
 import About from "./components/About/about";
-import Collection from "./components/Collection/collection";
+import CollectionMapper from "./components/Collection/CollectionMapper";
 
 
 
@@ -19,6 +19,11 @@ import {
     Link,
 
 } from "react-router-dom"
+import ListPrinter from './components/ListPrinter';
+import CollectionItem from './components/CollectionItem';
+import CollectionItemName from './components/CollectionItemName';
+import CollectionItemNum from './components/CollectionItemNum';
+import CollectionItemCat from './components/CollectionItemCat';
 
 
 
@@ -26,23 +31,24 @@ import {
 
 const App = () => {
     const [items, setItems] = useState([])
+    const [users, setUsers] = useState([])
+    const userId = '62294a50f14944cdb3bef377'
+    // change to decruped ID later
 
     useEffect (()=>{
-        axios.get ('http://localhost:5000/api/items/62294a50f14944cdb3bef377/collectionItems').then (res => {setItems(res.items)})
+        axios.get(`http://localhost:5000/api/items/${userId}/collectionItems`).then (res => {setItems(res.data)})
+        axios.get('http://localhost:5000/api/users').then (res => {setUsers(res.data)})
+        console.log(items)
     }, [])
 
     return ( 
             // <BrowserRouter>
             //     Hello World!
                 <div>
-                                    <Collection item = {items} />
+                    <h1>My Stuff here!</h1>
+                    <CollectionMapper itemList = {items} /> 
+                    <ListPrinter items = {items} resourceName1="item" resourceName2="item" resourceName3="item" itemComponent1={CollectionItemName} itemComponent2={CollectionItemCat} itemComponent3={CollectionItemNum}/>
 
-                                {/* <Routes>
-                                    <Route exact path="/" element={<Collection item = {items} />} />
-                                    <Route exact path="/" element={<Login  />} />
-                                    <Route exact path="/" element={<Login  />} />
-                                    <Route exact path="*" element={<Login  />} />
-                                </Routes> */}
                     
                 </div>
             // </BrowserRouter> 
@@ -52,3 +58,10 @@ const App = () => {
 export default App;
 
 
+                                //  <Routes>
+                                //     <Route exact path="/" element={<Collection item = {items} />} />
+                                //     <Route exact path="/" element={<About  />} />
+                                //     <Route exact path="/" element={<Photos  />} />
+                                //     <Route exact path="*" element={<Past  />} />
+                                //     <Route exact path="*" element={<Future  />} />
+                                // </Routes> 
